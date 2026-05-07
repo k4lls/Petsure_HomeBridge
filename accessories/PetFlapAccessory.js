a6792d93fe3d9953609b946e4cc2875be977f44f
asc-short <50656775+asc-short@users.noreply.github.com>
2026-05-07 21:01:05 +0100

    Update PetFlapAccessory.js

--- accessories/PetFlapAccessory.js
+++ accessories/PetFlapAccessory.js
@@ -1,38 +1,34 @@
 'use strict';
-
 const BaseAccessory = require('./BaseAccessory');
 
 class PetFlapAccessory extends BaseAccessory {
   constructor(log, accessory, device, session, config) {
     super(log, accessory, device, session);
-
     this.lock = device;
     this.config = config || {};
+    this.cachedStatus = null;
 
     this.service = this.accessory.getService(global.Service.LockMechanism);
-
     this.service
       .getCharacteristic(global.Characteristic.LockCurrentState)
       .on('get', this.getLockState.bind(this));
-
     this.service
       .getCharacteristic(global.Characteristic.LockTargetState)
       .on('set', this.setLockState.bind(this))
       .on('get', this.getLockState.bind(this));
 
-    this.battery = this.accessory.getService(global.Service.BatteryService);
+    // Support both Homebridge v1 (BatteryService) and v2 (Battery)
+    const BatteryServiceType = global.Service.Battery || global.Service.BatteryService;
+    this.battery = this.accessory.getService(BatteryServiceType);
     if (!this.battery) {
-      this.battery = this.accessory.addService(global.Service.BatteryService);
+      this.battery = this.accessory.addService(BatteryServiceType);
     }
-
     this.battery
       .getCharacteristic(global.Characteristic.BatteryLevel)
       .on('get', this.getBatteryLevel.bind(this));
-
     this.battery
       .getCharacteristic(global.Characteristic.ChargingState)
       .on('get', this.getBatteryChargeState.bind(this));
-
     this.battery
       .getCharacteristic(global.Characteristic.StatusLowBattery)
       .on('get', this.getBatteryLowLevel.bind(this));
@@ -42,33 +38,33 @@ class PetFlapAccessory extends BaseAccessory {
 
   pollStatus(data) {
     const devices = data && data.data && Array.isArray(data.data.devices) ? data.data.devices : [];
-
     for (const device of devices) {
       if (String(device.id) !== String(this.lock.id) || !device.status || !device.status.locking) {
         continue;
       }
-
+      this.cachedStatus = device.status;
       const state = device.status.locking.mode >= 1 ? 1 : 0;
-
       this.service
         .getCharacteristic(global.Characteristic.LockTargetState)
-        .setValue(state, null, 'internal');
-
+        .updateValue(state);
       this.service
         .getCharacteristic(global.Characteristic.LockCurrentState)
-        .setValue(state, null, 'internal');
-
+        .updateValue(state);
       return;
     }
   }
 
   getLockState(callback) {
+    if (this.cachedStatus && this.cachedStatus.locking) {
+      callback(null, this.cachedStatus.locking.mode > 0);
+      return;
+    }
     this.session.getLockStatus(this.lock.id, (data) => {
       if (!data || !data.status || !data.status.locking) {
         callback(new Error('Unable to load Sure Petcare lock status.'));
         return;
       }
-
+      this.cachedStatus = data.status;
       callback(null, data.status.locking.mode > 0);
     });
   }
@@ -78,33 +74,36 @@ class PetFlapAccessory extends BaseAccessory {
       callback(null);
       return;
     }
-
     let lockMode = targetState;
     if (targetState !== 0) {
       lockMode = this.config.lock_mode || targetState;
     }
-
     this.session.setLock(this.lock.id, lockMode, (data) => {
       if (!data) {
         callback(new Error('Unable to update Sure Petcare lock state.'));
         return;
       }
-
+      if (this.cachedStatus) {
+        this.cachedStatus.locking = { mode: lockMode };
+      }
+      callback(null);
       this.service
         .getCharacteristic(global.Characteristic.LockCurrentState)
-        .setValue(targetState, null, 'internal');
-
-      callback(null);
+        .updateValue(targetState);
     });
   }
 
   getBatteryLevel(callback) {
+    if (this.cachedStatus) {
+      callback(null, this.session.translateBatteryToPercent(this.cachedStatus.battery));
+      return;
+    }
     this.session.getLockStatus(this.lock.id, (data) => {
       if (!data || !data.status) {
         callback(new Error('Unable to load Sure Petcare battery level.'));
         return;
       }
-
+      this.cachedStatus = data.status;
       callback(null, this.session.translateBatteryToPercent(data.status.battery));
     });
   }
@@ -114,12 +113,17 @@ class PetFlapAccessory extends BaseAccessory {
   }
 
   getBatteryLowLevel(callback) {
+    if (this.cachedStatus) {
+      const battery = this.session.translateBatteryToPercent(this.cachedStatus.battery);
+      callback(null, battery <= 25 ? 1 : 0);
+      return;
+    }
     this.session.getLockStatus(this.lock.id, (data) => {
       if (!data || !data.status) {
         callback(new Error('Unable to load Sure Petcare battery level.'));
         return;
       }
-
+      this.cachedStatus = data.status;
       const battery = this.session.translateBatteryToPercent(data.status.battery);
       callback(null, battery <= 25 ? 1 : 0);
     });
